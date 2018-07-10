ead25d2687f717a66eb451e5730f73f9359fec15
xiaolong <dragonet_d@163.com>
2018-07-10 22:25:37 +0800

    tuple study over

--- tuple.py
+++ tuple.py
@@ -16,5 +16,53 @@ print(type(tup4))
 
 # 访问元组
 tuple1 = (123, 222, '123123', 'google')
+tuple2 = (1,)
 print(tuple1[0])
 print(tuple1[2:5])
+
+# 修改元组
+print(tuple1 + tuple2)
+
+# 元组运算符
+tupleForCalc = ('Google')
+tupleForCalc2 = ('Google')
+len(tupleForCalc)
+tupleForCalc3 = tupleForCalc + tupleForCalc2
+print(tupleForCalc3 * 4)
+isTrue = 3 in (1, 2, 3) # 元素是否存在 True
+for x in tuple1:  # 迭代
+    print(x)
+
+# 元组索引, 截取
+
+L = ('Google', 'Taobao', 'Runoob')
+print(L[2])
+print(L[-2])
+print(L[1:])
+
+# 元组内置函数
+
+len(tupleForCalc)
+max(tupleForCalc)
+min(tupleForCalc)
+tuple([1, 2, 3]) # 把列表转换为元组
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
