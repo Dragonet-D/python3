d5675fb8140ba7c7e8004e74b88a4ebca59b446c
xiaolong <dragonet_d@163.com>
2018-07-12 00:13:03 +0800

    cycle

--- cycle.py
+++ cycle.py
@@ -51,29 +51,94 @@ for i in range(5, 9):
     也可以使rang以指定数字开始并指定不用的增量(甚至可以是负数, 有时这也叫做'步长')
 '''
 for i in range(0, 10, 3):
-    print(i)
+    print(i) # 0 3 6 9
 
+for i in range(-10, -100, -30):
+    print(i) # -10 -40 -70
 
+a = ['Google', 'Baidu', 'Runoob', 'Taobao', 'QQ']
+for i in range(len(a)):
+    print(i, a[i])
 
+print(list(range(5))) # [0, 1, 2, 3, 4]
 
+# break和continue语句及循环中的else字句
+'''
+    break 语句可以跳出 for 和 while 的循环体。如果你从 for 或 while 循环中终止，任何对应的循环 else 块将不执行。
+'''
+for letter in 'Runoob':
+    if letter == 'b':
+        break
+    print(letter)
 
+var1 = 10
+while var1 > 0:
+    print(var1)
+    var1 = var1 - 1
+    if var1 == 5:
+        break
 
+print('Good bye!')
 
+'''
+    continue语句被用来告诉Python跳过当前循环块中的剩余语句，然后继续进行下一轮循环
+'''
 
+for letter in 'Runoob':
+    if letter == 'o':
+        continue
+    print(letter)
 
+var2 = 10
+while var2 > 0:
+    var2 -= 1
+    if var2 == 5:
+        continue
+    print(var2)
 
+print('Good bye')
 
+'''
+    循环语句可以有 else 子句，它在穷尽列表(以for循环)或条件变为 false (以while循环)导致循环终止时被执行,但循环被break终止时不执行
+'''
 
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, '等于', x, '*', n//x)
+            break
+    else:
+        # 循环中没有找到元素
+        print(n, '是质数')
 
+# pass 语句
+'''
+    Python pass是空语句，是为了保持程序结构的完整性。
+    pass 不做任何事情，一般用做占位语句
+'''
+while True:
+    pass
+    break
 
+sequence = [12, 34, 34, 23, 123, 3123]
+for i, j in enumerate(sequence):
+    print(i, j)
 
+n = 0
+summ = 0
+for n in range(0, 101): # n 范围0-100
+    summ += n
+print(summ)
 
-
-
-
-
-
-
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        mut = j*i
+        print('%d * %d = %%d'%(j,i,mut), end=' ')
+        j += 1
+    print('')
+    i+=1
 
 
 
